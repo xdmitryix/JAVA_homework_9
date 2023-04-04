da35bd1db1d8b6bdcf732271cae2e1ebf458e74b
dmitry <rbdeman@yandex.ru>
2023-04-04 23:54:11 +0300

    готово

--- Main.java
+++ Main.java
@@ -1,11 +1,28 @@
 // Создать список по аналогии LinkedList (список связанных элементов), реализовать в нем iterable интерфейс. 
 // Список должен содержать элементы со ссылкой на следующий элемент (если показалось мало, то реализовать ссылку и на предыдущий элемент)
-/**
- * Main
- */
-public class Main {
 
+import java.util.Iterator;
+
+public class Main {
     public static void main(String[] args) {
-        
+ 
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.add("data1");
+        list.add("data2");
+        list.add("data3");
+        list.add("data4");
+        list.add("data5");
+        list.add("data6");
+ 
+        list.add(1, "data2_1");
+        list.add(4, "data4_1");
+ 
+ 
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            String element = iterator.next();
+            System.out.println(element);
+        }
+ 
     }
 }
